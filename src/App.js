a0c1da5edea3716699f1057e43f57f8ac012c0e3
Ankur <ankurmishra710@gmail.com>
2022-09-29 18:12:30 +0530

    some

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Navbar from './Components/Navbar';
+import MainRoutes from './Pages/MainRoutes';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <MainRoutes />
     </div>
   );
 }
