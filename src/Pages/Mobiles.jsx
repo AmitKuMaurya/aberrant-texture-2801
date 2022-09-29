993bba4e6b688b069faead2c301cfe10dcfb48d2
Ankur <ankurmishra710@gmail.com>
2022-09-29 16:12:56 +0530

    create mobiles page , single MobilePage and Login page

---
+++ src/Pages/Mobiles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mobiles = () => {
+  return (
+    <div>Mobiles</div>
+  )
+}
+
+export default Mobiles
---
+++ src/Pages/SingleMobile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleMobile = () => {
+  return (
+    <div>SingleMobile</div>
+  )
+}
+
+export default SingleMobile
