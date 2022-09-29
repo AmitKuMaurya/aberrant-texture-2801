b14907c171e7368c637b1aa029f3c6dca63ff033
Ankur <ankurmishra710@gmail.com>
2022-09-29 14:21:10 +0530

    modified in action and reducer

--- src/Redux/app/action.js
+++ src/Redux/app/action.js
@@ -1,19 +1,17 @@
 import axios from "axios";
 import * as types from "./actionTypes";
 
-const getMobileRequest = () => {
-    return {
 
-    }
+const getMobiles = () => (dispatch) => {
+  dispatch({ type: types.GET_MOBILES_REQUEST });
+  return axios
+    .get("http://localhost:8080/mobiles")
+    .then((r) => {
+      dispatch({ type: types.GET_MOBILES_SUCCESS, payload: r.data });
+    })
+    .catch((e) => {
+      dispatch({ type: types.GET_MOBILES_FAILURE, payload: e });
+    });
 };
 
-const getMobiles = () => dispatch => {
-    dispatch({type: types.GET_MOBILES_REQUEST})
-    return axios.get("http://localhost:8080/mobiles").then(r=> {
-        dispatch({type: types.GET_MOBILES_SUCCESS, payload: r.data})
-    }).catch(e=> {
-        dispatch({type: types.GET_MOBILES_FAILURE, payload: e})
-    })
-}
-
-export { getMobiles }
+export { getMobiles };
