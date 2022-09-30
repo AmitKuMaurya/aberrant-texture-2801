b6e0902c8c74473f643c4bd1f3352c687ba17718
AmitKuMaurya <amit69maurya69@gmail.com>
2022-09-30 16:04:35 +0530

    dat 3 task completed

---
+++ src/Sell_Components/AdsCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdsCard() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default AdsCard
---
+++ src/Sell_Components/AdsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdsList() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default AdsList
