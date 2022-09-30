6cd0bec4121d2e5a651a316f5dc23386c372966c
Ankur <ankurmishra710@gmail.com>
2022-09-30 10:48:56 +0530

    sorting functionality

--- src/Components/FilterComp.jsx
+++ src/Components/FilterComp.jsx
@@ -4,7 +4,11 @@ import { useSearchParams } from "react-router-dom";
 const FilterComp = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const initialStateFilters = searchParams.getAll("state");
+  const initialSortBy = searchParams.getAll("sortBy");
   const [state, setState] = useState(initialStateFilters || []);
+
+  const [sortBy, setSortBy] = useState(initialSortBy[0] || "");
+
   const handleFilterCheckbox = (e) => {
     const newStates = [...state];
     if (newStates.includes(e.target.value)) {
@@ -14,15 +18,19 @@ const FilterComp = () => {
     }
     setState(newStates);
   };
+  const handleSort = (e) => {
+    setSortBy(e.target.value);
+  };
   useEffect(() => {
-    if (state) {
-      
+    if (state || sortBy) {
       let params = {};
       state && (params.state = state);
-      
+      sortBy && (params.sortBy = sortBy);
+
       setSearchParams(params);
     }
-  }, [state, setSearchParams]);
+  }, [state, setSearchParams, sortBy]);
+  console.log("SortByValue", sortBy);
   return (
     <div style={{ textAlign: "left" }}>
       <h3>FilterComp</h3>
@@ -72,6 +80,35 @@ const FilterComp = () => {
           />
           <label>Maharatra</label>
         </div>
+      </div>
+
+      <h3>Sort by Price</h3>
+      <div onChange={handleSort}>
+        <input
+          type="radio"
+          value=""
+          name="sortBy"
+          defaultChecked={sortBy === ""}
+        />
+        <label>Relevance</label>
+        <br />
+
+        <input
+          type="radio"
+          value="asc"
+          name="sortBy"
+          defaultChecked={sortBy === "asc"}
+        />
+        <label>Low to High</label>
+        <br />
+
+        <input
+          type="radio"
+          value="desc"
+          name="sortBy"
+          defaultChecked={sortBy === "desc"}
+        />
+        <label>High to Low</label>
       </div>
     </div>
   );
